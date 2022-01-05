24222fd83a69a30e15b3ae9bd33c4bb4f91ee18b
Deepu178 <deependrarajpoo178@gmail.com>
2022-01-05 21:51:23 +0530

    Second javascript file

---
+++ js8.js
@@ -0,0 +1,22 @@
+var name="Deependra"
+console.log("name",name)
+
+var secondName="secondname"
+console.log("secondname",secondname)
+
+name="Ruchi"
+console.log("name", name)
+console.log("secondname",secondname)
+
+var person={
+    name:"gauree",
+    age:20
+}
+console.log("person:",person)
+
+var secondPerson=person;
+console.log("secondPerson",secondPerson)
+
+person.name="Ruchi"
+console.log("person:",person)
+console.log("secondPerson",secondPerson)
